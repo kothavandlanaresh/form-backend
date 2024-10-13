3e5ebe4af834e53dfffa26d4aa8f9a924402622f
kothavandlanaresh <kothavandlanaresh@gmail.com>
2024-10-13 18:56:07 +0000

    first push

---
+++ server.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/send-email', (req, res) => {
+    const { subject, email, message, hireDate } = req.body;
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER, // Use environment variable
+            pass: process.env.EMAIL_PASS  // Use environment variable
+        }
+    });
+
+    const mailOptions = {
+        from: email,
+        to: process.env.EMAIL_USER, // Use environment variable
+        subject: `New message from ${email}: ${subject}`,
+        text: `Message: ${message}\nPreferred Date: ${hireDate}`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).send(error.toString());
+        }
+        res.status(200).send('Email sent: ' + info.response);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
